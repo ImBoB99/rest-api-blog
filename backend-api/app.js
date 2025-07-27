aec9727ef1891129d0a7aa1633cbaa7684714cba
Dominik Ubuntu <119432963+ImBoB99@users.noreply.github.com>
2025-07-27 11:47:41 +0200

    added express

--- backend-api/app.js
+++ backend-api/app.js
@@ -1,1 +1,9 @@
-console.log("Express App")
+require("dotenv").config({quiet: true})
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.APP_PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
